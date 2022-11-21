e0c391771967ae59c023a779083ec06d182a4b1a
PARAMESK <parameshk231@gmail.com>
2022-11-21 22:38:26 +0530

    form submit

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
 import reportWebVitals from './reportWebVitals';
 
+import Register from './pages/Register/Register';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
     //<App />
-    <Home />
+   // <Home />
     //<Cartpage />
+    <Register />
  // </React.StrictMode>
 );
 
